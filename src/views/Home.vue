093bf672b24d8b043b527660ef29856726538689
yangyimei <wsmis@vip.qq.com>
2017-08-14 15:32:14 +0800

    修改组件里文章内容

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <section>
-        <ibr-header-component desc="头部组件"></ibr-header-component>
+        <ibr-header-component></ibr-header-component>
         <div class="main-content">
             <i-row>
                 <i-col span="5">&nbsp;</i-col>
@@ -9,117 +9,8 @@
                     <i-col span="14">
                         <div id="article">
                             <ul>
-                                <li>
-                                    <div class="article-item">
-                                        <a href="#" class="warp-img">
-                                            <img src="http://upload-images.jianshu.io/upload_images/2088574-5421622746fd7c95.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/375/h/300" alt="" width="150" height="120">
-                                        </a>
-                                        <h3 class="title">画了576张画，写了496页手帐，从此生活就像开了挂</h3>
-                                        <p class="abstract">
-                                            这是我产后回归更新的第六篇文章。 每天粉丝的增长速度将近200，一直关注我的人都清楚我在怀孕期间一直坚持画画和写手帐，到了后期由于肚子比较大，坐在桌前会很难受，时不时要起来活</p>
-                                        <div class="meta">
-                                            <span>2017/08/14 14:11</span>
-                                            <a href="#"><icon type="ios-eye" size="18"></icon>13556</a>
-                                            <a href="#"><icon type="chatbox-working" size="18"></icon>243</a>
-                                            <a href="#"><icon type="ios-heart" size="18"></icon>700</a>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="article-item">
-                                        <a href="#" class="warp-img">
-                                            <img src="http://upload-images.jianshu.io/upload_images/6126137-49a845f051e90680.jpeg?imageMogr2/auto-orient/strip|imageView2/1/w/375/h/300" alt="" width="150" height="120">
-                                        </a>
-                                        <h3 class="title">可笑的巨婴，不懂放手的父母精心养出的废柴</h3>
-                                        <p class="abstract">
-                                            “我都27岁了，也经济独立，只是住在父母家，可父母就为自己设置门禁，不按时回家就连环夺命CALL，请问该如何沟通？”这是一位网友向《说说而已》说法家们的求助。 01/ 恭喜这</p>
-                                        <div class="meta">
-                                            <span>2017/08/14 14:11</span>
-                                            <a href="#"><icon type="ios-eye" size="18"></icon>13556</a>
-                                            <a href="#"><icon type="chatbox-working" size="18"></icon>243</a>
-                                            <a href="#"><icon type="ios-heart" size="18"></icon>700</a>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="article-item">
-                                        <a href="#" class="warp-img">
-                                            <img src="http://upload-images.jianshu.io/upload_images/62537-d1fa2eac4c976489.JPG?imageMogr2/auto-orient/strip|imageView2/1/w/375/h/300" alt="" width="150" height="120">
-                                        </a>
-                                        <h3 class="title">这可能是你看过的星空延时拍摄中最简单粗暴的教程</h3>
-                                        <p class="abstract">
-                                            前阵子在新疆时，在赛里木湖畔拍星空，因为有通宵的打算，于是干脆拍了几个小时的延时，最后简单生成一个10秒的延时星空发了个朋友圈，大家都挺喜欢的。点击下面可以看看： 结果有朋友</p>
-                                        <div class="meta">
-                                            <span>2017/08/14 14:11</span>
-                                            <a href="#"><icon type="ios-eye" size="18"></icon>13556</a>
-                                            <a href="#"><icon type="chatbox-working" size="18"></icon>243</a>
-                                            <a href="#"><icon type="ios-heart" size="18"></icon>700</a>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="article-item">
-                                        <a href="#" class="warp-img">
-                                            <img src="http://upload-images.jianshu.io/upload_images/6160220-dc85f87d099956e9?imageMogr2/auto-orient/strip|imageView2/1/w/375/h/300" alt="" width="150" height="120">
-                                        </a>
-                                        <h3 class="title">独自行走的路上，我们学会了告诉自己：I will be fine！</h3>
-                                        <p class="abstract">
-                                            Audrey是在新西兰陪伴我时间最久的一位朋友，几乎贯穿了我的整个回忆，想甩都甩不掉的那种。视频中和我一起在134米峡谷里荡秋千的那个姑娘，就是她。以下的故事，是2013年底</p>
-                                        <div class="meta">
-                                            <span>2017/08/14 14:11</span>
-                                            <a href="#"><icon type="ios-eye" size="18"></icon>13556</a>
-                                            <a href="#"><icon type="chatbox-working" size="18"></icon>243</a>
-                                            <a href="#"><icon type="ios-heart" size="18"></icon>700</a>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="article-item">
-                                        <a href="#" class="warp-img">
-                                            <img src="http://upload-images.jianshu.io/upload_images/1714291-78b7d57e434d614f.png?imageMogr2/auto-orient/strip|imageView2/1/w/375/h/300" alt="" width="150" height="120">
-                                        </a>
-                                        <h3 class="title">QQ音乐、网易云音乐｜版权向左、社交向右</h3>
-                                        <p class="abstract">
-                                            近日不少网易云用户发现自己收藏好多歌曲都下架了，点击相关曲目，会提示“因合作方要求，该资源暂时无法使用”。 之前网易云音乐下架了大量韩国歌曲，其中不乏bigbang、IU、鸟</p>
-                                        <div class="meta">
-                                            <span>2017/08/14 14:11</span>
-                                            <a href="#"><icon type="ios-eye" size="18"></icon>13556</a>
-                                            <a href="#"><icon type="chatbox-working" size="18"></icon>243</a>
-                                            <a href="#"><icon type="ios-heart" size="18"></icon>700</a>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="article-item">
-                                        <a href="#" class="warp-img">
-                                            <img src="http://upload-images.jianshu.io/upload_images/5403259-4076f3c3f3adc123.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/375/h/300" alt="" width="150" height="120">
-                                        </a>
-                                        <h3 class="title">【简书交友】水青衣：我是荒野的一截狼性藤蔓</h3>
-                                        <p class="abstract">
-                                            昵称：水青衣 地点：一个壮族人聚集的地方（是的，我是个壮族菇凉） 交友宣言：你是谁，便会遇见谁 自荐文章：《纸媒投稿丨我做杀手的这些年》《为什么拒绝我的打扰》《专访主编：一鸣</p>
-                                        <div class="meta">
-                                            <span>2017/08/14 14:11</span>
-                                            <a href="#"><icon type="ios-eye" size="18"></icon>13556</a>
-                                            <a href="#"><icon type="chatbox-working" size="18"></icon>243</a>
-                                            <a href="#"><icon type="ios-heart" size="18"></icon>700</a>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="article-item">
-                                        <a href="#" class="warp-img">
-                                            <img src="http://upload-images.jianshu.io/upload_images/5403259-4076f3c3f3adc123.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/375/h/300" alt="" width="150" height="120">
-                                        </a>
-                                        <h3 class="title">【简书交友】水青衣：我是荒野的一截狼性藤蔓</h3>
-                                        <p class="abstract">
-                                            昵称：水青衣 地点：一个壮族人聚集的地方（是的，我是个壮族菇凉） 交友宣言：你是谁，便会遇见谁 自荐文章：《纸媒投稿丨我做杀手的这些年》《为什么拒绝我的打扰》《专访主编：一鸣</p>
-                                        <div class="meta">
-                                            <span>2017/08/14 14:11</span>
-                                            <a href="#"><icon type="ios-eye" size="18"></icon>13556</a>
-                                            <a href="#"><icon type="chatbox-working" size="18"></icon>243</a>
-                                            <a href="#"><icon type="ios-heart" size="18"></icon>700</a>
-                                        </div>
-                                    </div>
+                                <li v-for="res in data">
+                                    <ibr-article-component :todo="res"></ibr-article-component>
                                 </li>
                             </ul>
                         </div>
@@ -134,14 +25,38 @@
 
 <script>
     import { Row as iRow, Col as iCol } from 'iview/src/components/grid';
-    import Icon from 'iview/src/components/icon';
     import ibrHeaderComponent from '../components/header.vue';
+    import ibrArticleComponent from '../components/article.vue';
     export default {
+        data: function () {
+            return {
+                data: [
+                    {
+                        imgUrl: 'http://upload-images.jianshu.io/upload_images/2088574-5421622746fd7c95.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/375/h/300',
+                        title: '画了576张画，写了496页手帐，从此生活就像开了挂',
+                        abstract: '这是我产后回归更新的第六篇文章。 每天粉丝的增长速度将近200，一直关注我的人都清楚我在怀孕期间一直坚持画画和写手帐，到了后期由于肚子比较大，坐在桌前会很难受，时不时要起来活',
+                        eye: '13556',
+                        msg: '243',
+                        heart: '700',
+                        addtime: '2017/08/14 14:11'
+                    },
+                    {
+                        imgUrl: 'http://upload-images.jianshu.io/upload_images/6126137-49a845f051e90680.jpeg?imageMogr2/auto-orient/strip|imageView2/1/w/375/h/300',
+                        title: '可笑的巨婴，不懂放手的父母精心养出的废柴',
+                        abstract: '“我都27岁了，也经济独立，只是住在父母家，可父母就为自己设置门禁，不按时回家就连环夺命CALL，请问该如何沟通？”这是一位网友向《说说而已》说法家们的求助。 01/ 恭喜这',
+                        eye: '5580',
+                        msg: '74',
+                        heart: '170',
+                        addtime: '2017/08/14 14:11'
+                    }
+                ]
+            }
+        },
         components: {
             iRow,
             iCol,
-            Icon,
-            ibrHeaderComponent
+            ibrHeaderComponent,
+            ibrArticleComponent
         }
     }
 </script>
